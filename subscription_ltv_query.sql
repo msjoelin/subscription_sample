78c6bccb3a5eef02b1216763c294ac24e123b93a
msjoelin <marcussjolin89@gmail.com>
2025-02-23 14:44:50 +0100

    Update subscription_ltv_query.sql

--- subscription_ltv_query.sql
+++ subscription_ltv_query.sql
@@ -1,30 +1,48 @@
 
+
 with 
-
+--
 base as (
-SELECT 
-user_id,
-min(date_trunc(start, month)) over (partition by user_id) as cohort_month,  
-sub_id,  
-start, 
-tier, 
-price
-FROM `general-319511.dwh.subscription_sample` -- Insert your table 
+	select 
+	user_id, 
+	sub_id, 
+	start, 
+	date_trunc(start, month) as start_month, 
+	tier, 
+	price 
+	from general-319511.dwh.subscription_sample
+--	limit 100 
 )
-
-, base2 as (
-select user_id, cohort_month, 
-date_diff(date_trunc(start, month), cohort_month, month) as user_month, 
-start, 
-tier, price
+--
+,base2 as (
+select *, 
+min(start_month) over (partition by user_id)
+as cohort_month
 from base
 )
-
+,base3 as (
+select *, 
+date_diff(start, cohort_month, MONTH)
+as user_month
+from base2 
+)
+,base4 as (
 select 
 cohort_month, 
 user_month, 
 count(distinct user_id) as users, 
-sum(price) as revenue  
-from base2 
+sum(price) as revenue
+from base3
 group by 1,2 
 order by 1,2 
+)
+,base5 as (
+select *, 
+sum(revenue) over 
+(partition by cohort_month order by user_month)
+as revenue_cumulative
+from base4
+)
+select * from base5 
+order by cohort_month, user_month 
+--limit 1000 
